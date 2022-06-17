0aa9b9bb7bd179f789618c4ed18d8160d355bb35
Ana Paula Ferreira Fernandes <72840444+anapaulaff00@users.noreply.github.com>
2022-06-17 11:17:06 -0300

    create validacoes.js

---
+++ validacoes.js
@@ -0,0 +1,151 @@
+<!-- CPF-->
+    <script>
+         // Mascara de texto CPF	
+        function mascara(i){
+            var v = i.value;
+            
+            if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+                i.value = v.substring(0, v.length-1);
+                return;
+            }
+            
+            i.setAttribute("maxlength", "14");
+            if (v.length == 3 || v.length == 7) i.value += ".";
+            if (v.length == 11) i.value += "-";
+        }
+
+
+        // Validar CPF	
+        function validarCPF(cpf) {	
+            cpf = cpf.replace(/[^\d]+/g,'');	
+            if(cpf == '') return false;	
+            // Elimina CPFs invalidos conhecidos	
+            if (cpf.length != 11 || 
+                cpf == "00000000000" || 
+                cpf == "11111111111" || 
+                cpf == "22222222222" || 
+                cpf == "33333333333" || 
+                cpf == "44444444444" || 
+                cpf == "55555555555" || 
+                cpf == "66666666666" || 
+                cpf == "77777777777" || 
+                cpf == "88888888888" || 
+                cpf == "99999999999")
+                    return alert('CPF Invalido');		
+            // Valida 1o digito	
+            add = 0;	
+            for (i=0; i < 9; i ++)		
+                add += parseInt(cpf.charAt(i)) * (10 - i);	
+                rev = 11 - (add % 11);	
+                if (rev == 10 || rev == 11)		
+                    rev = 0;	
+                if (rev != parseInt(cpf.charAt(9)))		
+                    return alert('CPF Invalido');		
+            // Valida 2o digito	
+            add = 0;	
+            for (i = 0; i < 10; i ++)		
+                add += parseInt(cpf.charAt(i)) * (11 - i);	
+            rev = 11 - (add % 11);	
+            if (rev == 10 || rev == 11)	
+                rev = 0;	
+            if (rev != parseInt(cpf.charAt(10)))
+                return alert('CPF Invalido');		
+            return true;   
+        }
+    </script>
+
+<!-- VALIDAÇÃO SENHA-->
+
+    <script>
+        //Verifica se as senhas são iguais
+		let senha = document.getElementById('password1');
+		let senhaC = document.getElementById('password2');
+
+		function validarSenha() {
+		if (senha.value != senhaC.value) {
+			senhaC.setCustomValidity("Senhas diferentes!");
+                $('#Button').prop('disabled', true);
+			senhaC.reportValidity();
+			return false;
+		} else {
+			senhaC.setCustomValidity("");
+                $('#Button').prop('disabled', false);
+			return true;
+		}
+		}
+
+        // Verificar quando o campo for modificado, para que a mensagem suma quando as senhas forem iguais
+        senhaC.addEventListener('input', validarSenha);
+    </script>
+
+
+    <!-- CONSULTA CEP AUTOMATICO-->
+    <script>
+    
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+           
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+           
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor;
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+                
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
